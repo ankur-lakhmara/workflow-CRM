f30f30149c04ad97a55ad0bccda6c861ec540850
Ankur <59440628+ankur-lakhmara@users.noreply.github.com>
2026-02-08 21:54:24 +0530

    added custom runtime exception handler

--- src/main/java/org/developerport/workflowcrmbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/org/developerport/workflowcrmbackend/exception/GlobalExceptionHandler.java
@@ -1,33 +1,33 @@
-//package org.developerport.workflowcrmbackend.exception;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-////package org.developerport.workflowcrmbackend.dto.exception;
-//
-//import org.developerport.workflowcrmbackend.dto.exception.ErrorResponse;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//    @ExceptionHandler(RuntimeException.class)
-//    public ResponseEntity<ErrorResponse> handleRunTimeException(RuntimeException ex){
-//        ErrorResponse error = new ErrorResponse(
-//                ex.getMessage(),
-//                HttpStatus.BAD_REQUEST.value(),
-//                System.currentTimeMillis()
-//        );
-//        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-//    }
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
-//
-//        ErrorResponse error = new ErrorResponse(
-//                "Something went wrong",
-//                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-//                System.currentTimeMillis()
-//        );
-//
-//        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-//}
+package org.developerport.workflowcrmbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+//package org.developerport.workflowcrmbackend.dto.exception;
+
+import org.developerport.workflowcrmbackend.dto.exception.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRunTimeException(RuntimeException ex){
+        ErrorResponse error = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                "Something went wrong",
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
